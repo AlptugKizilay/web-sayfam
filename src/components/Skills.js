27a8c007766dedc2f2d729429347dc818313d89b
AlptugKizilay <alptugkizilay@gmail.com>
2023-07-07 02:54:26 +0300

    skills part is updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,13 +7,13 @@ const Skills = () => {
   const { t } = useTranslation();
   return (
     <div className="font-inter h-110 dark:bg-graydi dark:text-white">
-      <h2 className=" text-4xl tracking-wide font-semibold mt-16 " >{t("Skills")}</h2>
-      <div className=" flex justify-center space-x-6 mt-16 ">
+      <h2 className=" text-3xl lg:text-4xl tracking-wide font-semibold mt-4 lg:mt-16 " >{t("Skills")}</h2>
+      <div className=" flex justify-center flex-wrap  mt-4 lg:mt-16 ">
         {persData.skills.map((item, index) => {
           return (
-            <div key={index} className="flex flex-col justify-center items-center" >
-              <img alt={item.skill} src={item.img} className=" w-32 h-32 rounded-md"></img>
-              <h5 className=" text-2xl font-medium text-grayki dark:text-grr mt-2 "> {item.skill} </h5>
+            <div key={index} className="flex flex-col justify-center items-center px-4 w-32 h-32 lg:w-64 lg:h-64" >
+              <img alt={item.skill} src={item.img} className=" w-16 h-16 lg:w-32 lg:h-32 rounded-md"></img>
+              <h5 className=" text-base lg:text-2xl font-medium text-grayki dark:text-grr mt-2 "> {item.skill} </h5>
             </div>
           );
         })}
