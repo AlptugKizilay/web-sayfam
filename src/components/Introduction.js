ec2ecd9fab8b2e491e6b406054b8df7a12c0e07b
AlptugKizilay <alptugkizilay@gmail.com>
2023-07-07 02:35:19 +0300

    intro parts are changed

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -11,29 +11,29 @@ const Intorduction = ({ changeLanguage, language }) => {
   const { t } = useTranslation();
 
   return (
-    <div className="bg-wild-sand dark:bg-blackdi dark:text-white flex flex-col justify-center px-60 pb-10 font-inter">
+    <div className="bg-wild-sand dark:bg-blackdi dark:text-white flex flex-col justify-center px-0 lg:px-60 pb-10 lg:pb-10 font-inter">
       <div>
         <Header changeLanguage={changeLanguage} language={language} />
       </div>
-      <div className="flex mt-16 ">
-        <div className="flex-col ml-44 mr-20">
-          <h3 className="font-normal text-3xl tracking-widest text-start mb-3">
+      <div className="flex mt-8 lg:mt-16 ">
+        <div className="flex-col ml-4 lg:ml-44 mr-2 lg:mr-20">
+          <h3 className="font-normal text-3xl tracking-widest text-start mb-1 lg:mb-3">
             {t("greeting")} 👋
           </h3>
-          <p className="not-italic font-semibold text-start text-4.5xl mb-12 tracking-widestt ">
+          <p className="not-italic font-semibold text-start text-1xl lg:text-4.5xl mb-6 lg:mb-12 tracking-widestt ">
             {t("I")} {persData.name}. {t("intro")}
             <br /> {t("Let's meet!")}{" "}
           </p>
           <nav className="flex space-x-4 mb-6">
             <a href={link.linkedin}>
-              <FontAwesomeIcon className="w-10 h-10" icon={faLinkedin} />
+              <FontAwesomeIcon className="w-7 h-7 lg:w-10 lg:h-10" icon={faLinkedin} />
             </a>
             <a href={link.git}>
-              <FontAwesomeIcon className="w-10 h-10" icon={faGithub} />
+              <FontAwesomeIcon className="w-7 h-7 lg:w-10 lg:h-10" icon={faGithub} />
             </a>
           </nav>
 
-          <p className="not-italic font-medium  text-start text-1.5xl tracking-wider">
+          <p className="not-italic font-normal lg:font-medium text-start text-base lg:text-1.5xl tracking-tight  lg:tracking-wider">
             {t("Currently")}{" "}
             <span className="text-pinkki">{t("Freelancing")}</span> {t("for")}{" "}
             <span className="text-pinkki">{t("UX")}</span> {t("Project")}
@@ -47,11 +47,11 @@ const Intorduction = ({ changeLanguage, language }) => {
             </a>
           </p>
         </div>
-        <div className=" w-96 h-96 mr-44 flex">
-          <div className="  bg-amaranth w-96 h-96 p-48 max-w-max rounded-3xl flex"></div>
+        <div className=" w-24 lg:w-96 h-24 lg:h-96 mr-4 lg:mr-44 mt-12 lg:mt-1 flex">
+          <div className="  bg-amaranth w-24 lg:w-96 h-24 lg:h-96 p-12 lg:p-48 max-w-max rounded-3xl flex"></div>
           <img
             alt="photo"
-            className="w-96 h-96  max-w-max rounded-3xl absolute m-5 z-10 "
+            className="w-24 lg:w-96 h-24 lg:h-96  max-w-max rounded-3xl absolute m-1 z-2 lg:m-5 lg:z-10 "
             src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
           />
         </div>
